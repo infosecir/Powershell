3cf6192f08323882a5bb35d7115863f43ce05446
infosecir <35237880+infosecir@users.noreply.github.com>
2018-06-12 13:31:39 -0500

    Update ConvertSIDtoUSER2.ps1

--- ConvertSIDtoUSER2.ps1
+++ ConvertSIDtoUSER2.ps1
@@ -1,3 +1,3 @@
-﻿$objSID = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-21-3496554487-1900509739-3140548133-500")
+﻿$objSID = New-Object System.Security.Principal.SecurityIdentifier("SID")
 $objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
 $objUser.Value
