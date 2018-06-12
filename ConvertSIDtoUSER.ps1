9ce5598de9daa92777a7cc19bb0c3535b2285f4e
infosecir <35237880+infosecir@users.noreply.github.com>
2018-06-12 13:31:16 -0500

    Update ConvertSIDtoUSER.ps1

--- ConvertSIDtoUSER.ps1
+++ ConvertSIDtoUSER.ps1
@@ -1,7 +1,7 @@
-﻿   $objSID = New-Object System.Security.Principal.SecurityIdentifier ("S-1-5-21-2807171826-104970049-2147198314-1003")
+﻿   $objSID = New-Object System.Security.Principal.SecurityIdentifier ("<SID>")
    $objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
    $objUser.Value
 
 
    Import-Module ActiveDirectory
-   Get-ADUser -Identity "S-1-5-21-2807171826-104970049-2147198314-1003"
+   Get-ADUser -Identity "<SID>"
